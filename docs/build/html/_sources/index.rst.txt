2c9be7e318e4dbb254e17b825df7c0c7dfbcd10d
Sagar Patel <sagar10patel@gmail.com>
2023-04-20 14:35:14 -0600

    Add Documentation

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,27 @@
+.. sphinxy_tut documentation master file, created by
+   sphinx-quickstart on Thu Apr 20 14:10:34 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinxy_tut's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+
+The Egyptian Sphinx
+--------------------
+
+.. image:: _static/sphinx.png
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sphinxy_tut'
+copyright = '2023, Sagar Patel'
+author = 'Sagar Patel'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+
+autoclass_content = 'both'
